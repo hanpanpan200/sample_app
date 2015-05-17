b6b54164bff2c54031ff7289d065ee47c12c31bb
fionhan <1056856566@qq.com>
2015-05-17 21:12:00 +0800

    Finish use signup

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.8'
+gem 'bootstrap-sass', '3.2.0.0'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use SCSS for stylesheets
@@ -22,6 +23,8 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'bcrypt','3.1.7'
+
+gem 'byebug'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid sign up information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: {name: "", email: "valid@example.com", password: "123", password_confirmation: "456"}
+    end
+    assert_template 'users/new'
+  end
+
+  test "valid sign up information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: {name: "pennyhan", email: "pennyhan20102010@sina.com", password: "password", password_confirmation: "password"}
+    end
+    assert_template 'users/show'
+  end
+
+end
